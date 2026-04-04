9467bfd0df706d1c69ada452ab2b472eff89731d
Khang Thai <61812548+kunfupen@users.noreply.github.com>
2026-04-03 20:49:21 -0700

    completed commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-TMDB_API_KEY = os.environ.get('TMBD_API_KEY')
+TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
 YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
 
 TMDB_BASE = 'https://api.themoviedb.org/3'
